eb0efae59b7b8c2e91a1775befeb0b0ae22c55bc
Abdullah <164920392+abdullahkhanx4455@users.noreply.github.com>
2024-05-04 21:00:08 +0500

    Add files via upload

---
+++ Class1.js
@@ -0,0 +1,8 @@
+"use strict";
+// assignment: 01: Calculate your age based on the current year and your birth year
+function calculateAge(curYear, birthyear) {
+    console.log("age:", curYear - birthyear);
+}
+let birthYear = 2004;
+let currYear = 2024;
+calculateAge(currYear, birthYear);
