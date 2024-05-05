14f7a6d002d6761bb90e41445591bedf293a783a
Abdullah <164920392+abdullahkhanx4455@users.noreply.github.com>
2024-05-05 21:05:28 +0500

    Add files via upload

---
+++ Class2.js
@@ -0,0 +1,9 @@
+
+// assignment: 02: Write a program that calculates the area of a rectangle using length and width variables.
+
+function area(lengths, width) {
+    console.log("Area of a rectangle =", lengths * width);
+}
+let lengths = 20;
+let width = 40;
+area(lengths, width);
