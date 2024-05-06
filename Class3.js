0f5ab978b8fcbd3d039548ecc0653a251e950c58
Abdullah <164920392+abdullahkhanx4455@users.noreply.github.com>
2024-05-06 22:13:29 +0500

    Add files via upload

---
+++ Class3.js
@@ -0,0 +1,8 @@
+"use strict";
+// assignment: 03: Write a program that calculates the area of a circle.
+function area2(radious, pai) {
+    console.log("area", pai * radious * radious);
+}
+let radious = 10;
+let pai = 3.14;
+area2(radious, pai);
