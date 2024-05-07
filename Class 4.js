e24d178531088ce16556905b42dfdf87914777c0
Abdullah <164920392+abdullahkhanx4455@users.noreply.github.com>
2024-05-07 20:44:12 +0500

    Add files via upload

---
+++ Class 4.js
@@ -0,0 +1,7 @@
+"use strict";
+// assignment: 04: Write a program that calculates the area of the cube.
+function cube(shape) {
+    console.log("cube", 6 * (shape * shape));
+}
+let shape = 5;
+cube(shape);
