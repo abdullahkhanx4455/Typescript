3bc74cf439c56fa7c4069d4a317179ebc96a2d70
Abdullah <164920392+abdullahkhanx4455@users.noreply.github.com>
2024-05-08 21:00:51 +0500

    Add files via upload

---
+++ Class 5.js
@@ -0,0 +1,7 @@
+"use strict";
+// assignment: 05: Create a program that converts a temperature from Fahrenheit to Celsius and vice versa using a variable.
+function temp(Fahrenheit) {
+    console.log("temp", (Fahrenheit - 32) / 1.8);
+}
+let Fahrenheit = 300;
+temp(Fahrenheit);
