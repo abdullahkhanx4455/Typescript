0cf3c4013610d585f1ff94dc09a22cdfb04bb613
Abdullah <164920392+abdullahkhanx4455@users.noreply.github.com>
2024-05-09 21:06:11 +0500

    Add files via upload

---
+++ Class 6.js
@@ -0,0 +1,7 @@
+"use strict";
+//assignment: 06:  Convert a given number of seconds into minutes and seconds using variables.
+function time(sec) {
+    console.log("time", sec / 60);
+}
+let sec = 3600;
+time(sec);
