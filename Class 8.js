9577a4e004c2b4635502caf54a66b62f1dff1eed
Abdullah <164920392+abdullahkhanx4455@users.noreply.github.com>
2024-05-12 21:35:26 +0500

    Add files via upload
    Sure! This code defines a function called 'check' that tells you if a number is even or odd. If you pass a number to the function, it will print "number is even" if it's even and "number is odd" if it's odd. When you use check(33), it prints "number is odd" because 33 is an odd number.

---
+++ Class 8.js
@@ -0,0 +1,11 @@
+"use strict";
+//Assignment:08: Using conditional statements, check if the number is Even or Odd
+function check(num) {
+    if (num % 2 == 0) {
+        console.log("number is even");
+    }
+    else {
+        console.log("number is odd");
+    }
+}
+check(33);
