72a9fad76fa2051d1ed0f61910ede97f272e86a1
Abdullah <164920392+abdullahkhanx4455@users.noreply.github.com>
2024-05-13 21:01:56 +0500

    Add files via upload
    This code defines a function called `checks` that examines a number. If the number is positive, it prints "number is positive". If it's negative, it prints "number is negative". If it's neither positive nor negative (in other words, it's 0), it prints "number is zero". Then, it calls the `checks` function with the number 10, so it prints "number is positive".

---
+++ Class 9.js
@@ -0,0 +1,16 @@
+"use strict";
+//Assignment:09: Using conditional statements check if the number is positive or negitive or zero
+function checks(num) {
+    if (num > 0) {
+        console.log("number is positive");
+    }
+    else if (num < 0) {
+        console.log("number is negtive");
+    }
+    else {
+        console.log("number is zero");
+    }
+    {
+    }
+}
+checks(10);
